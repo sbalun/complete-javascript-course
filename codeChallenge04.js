6094d42b23d951c43abe000148499ff5d7dde8d8
Scott Balun <sbalun@gmail.com>
2023-05-03 17:01:32 -0400

    Code challenge 04

--- codeChallenge04.js
+++ codeChallenge04.js
@@ -5,7 +5,7 @@ x---------------------x
 - In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
 
 Your tasks:
-    1. Calculate the tip,depending on the bill value.  Create a variable called 'tip'for this.
+    1. Calculate the tip depending on the bill value.  Create a variable called 'tip' for this.
         - It's not allowed to use an if/else statement 😅
         - If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!
 
@@ -19,3 +19,8 @@ Your tasks:
         - To calculate 20% of a value, simply multiply it by 20/100 = 0.2
         - Value X is between 50 and 300, if it's>= 50 && <= 300
  */
+
+const billValue = 100.00;
+let tip = billValue * .15;
+let totalValue = billValue + tip;
+console.log(`The bill was ${billValue}, the tip was ${tip}, and the total value ${totalValue}`)
